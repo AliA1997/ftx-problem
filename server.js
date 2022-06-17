71f548763ac286293378726f6b68c4c3889d5336
Ali Qassem Alhaddad <AliA1997@users.noreply.github.com>
2022-06-17 11:41:44 -0400

    implement-sqs-queue - Implemented sqs queue to place orders on aws.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const ftxApi = require("ftx-api");
+const awsSDK = require("aws-sdk");
 const express = require("express");
 const https = require("https");
 const crypto = require("crypto-js");
@@ -13,7 +14,37 @@ const cryptoAssetsToBuy = ["GRT", "ETH", "BTC", "SOL"];
 const cache = new NodeCache();
 cache.set("marketOrders", "[]", 1000 * 60 * 60);
 
-app.use(express.json());
+//Configure awsSdk options.
+awsSDK.config.update({
+  region: "us-east-1",
+  accessKeyId: "AKIAZRQDF3RWOTT7LWVQ",
+  secretAccessKey: "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",
+});
+
+const queueUrl = "https://sqs.us-east-1.amazonaws.com/656062930028/order-placement-queue.fifo";
+
+const createSendMessageParams = (messageTitle, messageBody) => ({
+  DelaySeconds: 0,
+  MessageAttributes: {
+    Title: {
+      DataType: "String",
+      StringValue: messageTitle,
+    },
+  },
+  MessageBody: JSON.stringify(messageBody),
+  MessageGroupId: "order-placement-queue.fifo",
+  QueueUrl: queueUrl,
+});
+
+const createReceiveMessageParams = () => ({
+  AttributeNames: ["SentTimestamp"],
+  MaxNumberOfMessages: 10,
+  MessageAttributeNames: ["All"],
+  QueueUrl: queueUrl,
+  VisibilityTimeout: 20,
+  WaitTimeSeconds: 0,
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -23,8 +54,11 @@ app.use((req, res, next) => {
     process.env.FTX_API_SECRET,
     { domain: "ftxus" }
   );
+  //Define new sqs queue.
+  const sqsQueue = new awsSDK.SQS({ apiVersion: "2012-11-05" });
   app.set("client", client);
   app.set("cache", cache);
+  app.set("queue", sqsQueue);
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -39,10 +73,9 @@ app.get("/api/check_orders", async (req, res) => {
     cache.get("marketOrders") ? cache.get("marketOrders") : "{}"
   );
   let currentMarketPricesResponse = await apiClient.getMarkets();
-  let currentMarketPrices = currentMarketPricesResponse.result.filter((m) =>
-    m.name === `GRT/USD`
-  )
-  .map(m => m.ask);
+  let currentMarketPrices = currentMarketPricesResponse.result
+    .filter((m) => m.name === `GRT/USD`)
+    .map((m) => m.ask);
 
   Promise.map(
     currentMarketPrices,
@@ -112,6 +145,7 @@ app.post("/api/place-order", async (req, res) => {
   );
   const apiClient = app.get("client");
   const cache = app.get("cache");
+  const queue = app.get('queue');
   try {
     if (!cryptoAssetBuyAndSell) throw new Error("Asset not available to buy.");
     const ordersToPlace = [
@@ -134,7 +168,9 @@ app.post("/api/place-order", async (req, res) => {
         reduceOnly: false,
       },
     ];
+    const params = createSendMessageParams("Place Order:", ordersToPlace);
     cache.set("marketOrders", JSON.stringify(ordersToPlace));
+    queue.sendMessage(params, (err, data) => console.log("Order Placed:", data));
     console.log(cache.get("marketOrders"));
     res.json({ success: true });
   } catch (e) {
